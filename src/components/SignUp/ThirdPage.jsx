bd7b0cd4a3256bf0c3e5642b175cd7aeac3f3c48
Aman Ibrahim <amanmibra@gmail.com>
2017-12-07 23:48:55 -0500

    Added image to ThirdPage component

--- src/components/SignUp/ThirdPage.jsx
+++ src/components/SignUp/ThirdPage.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
+import DMM_letters from './../../images/DMM_letters.png';
+
 class ThirdPage extends Component {
   render () {
     return (
       <div className='third-step'>
         <h2>Congratulations! You are offically signed up for Duke Major Mapper</h2>
         <h3>Click the Home Page button below to continue to the home page.</h3>
+        <img src={DMM_letters} alt="DMM_letters" height='200em'/>
       </div>
     );
   }
