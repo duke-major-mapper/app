3b6ccb9e7e2426d14cc0c05e5f6a57cc60cbe877
Aman Ibrahim <amanmibra@gmail.com>
2017-12-09 14:35:57 -0500

    Created login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+
+import logo from './../images/DMM_logo.png';
 
 export default class Login extends Component {
     render() {
       return (
-        <div>
-          Login Page: <br />
-          <Link to='/home'>
-            test
-          </Link>
+        <div className="signup">
+          <Card>
+            <div style={{ padding: '24px' }}>
+              <img style={{ display: 'inline-block' }} src={logo} alt="logo" height="90"/> <br/>
+              <TextField
+                hintText=""
+                floatingLabelText="Username"
+              /><br />
+              <TextField
+                hintText=""
+                floatingLabelText="Password"
+                type="password"
+              /><br /> <br />
+              <RaisedButton
+                label="Sign in"
+                primary={true}
+                // onClick={this.handleClick.bind(this)}
+              />
+            </div>
+          </Card>
         </div>
       );
     }
