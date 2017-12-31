2d6d88c0ea78f29704a7e5bfea6e0ffdaf7293ae
Aman Ibrahim <amanmibra@gmail.com>
2017-12-31 13:07:23 -0500

    Adding password hashing to API endpoints.

--- backend/src/Api/Login.js
+++ backend/src/Api/Login.js
@@ -1,47 +1,51 @@
 const app = require('../shared/Express').app;
 const template = require('../shared/Express').template;
 const connection = require('../shared/Connection');
-
+const sha256 = require('js-sha256');
 
 app.put('/login', function (req, res) {
     const id = req.body.netID;
-    const pw = req.body.password;
+    const password = req.body.password;
+    const hash = sha256(password + id);
 
     if (!id) {
         res.status(400).send("Please provide an id parameter");
         return;
     }
-    if (!pw) {
+    if (!password) {
         res.status(400).send("Please provide a password parameter");
         return;
     }
-
+    //TODO: Fix objects below
     connection.query(
         `SELECT * FROM User WHERE id LIKE '%${id}%'`, function (error, result) {
             if (error) {
-              res.status(500).send({
-                  success: false,
-                  msg: (error.sqlMessage ? error.sqlMessage : error)
-              });
+              template.success = false;
+              template.message = (error.sqlMessage ? error.sqlMessage : error);
+              template.status = 500;
+              res.status(500).send(template);
+              return;
             } else if (result.length < 1) {
-                res.status(400).send({
-                    success: false,
-                    msg: 'user id not found'
-                });
-            } else if (result[0].password !== pw) {
-                res.status(400).send({
-                    success: false,
-                    msg: 'incorrect password'
-                });
+                template.success = false;
+                template.message = 'User ID not found';
+                template.status = 400;
+                res.status(400).send(template);
+                return;
+            } else if (result[0].password_hash !== hash) {
+                template.success = false;
+                template.message = 'Incorrect Password';
+                template.status = 400;
+                res.status(400).send(template);
+                return;
             } else {
-              console.log('result',result);
-                res.status(200).send({
-                    success: true,
-                    netID: id,
-                    name: result[0].name,
-                    msg: 'login verified'
-                });
+              template.success = true;
+              template.netID = id;
+              template.name = result[0].name;
+              template.message = 'Login Verified'
+              template.status = 200;
+              res.status(200).send(template);
             }
+            return;
           }
     );
 
--- backend/src/Api/NewUser.js
+++ backend/src/Api/NewUser.js
@@ -1,12 +1,14 @@
 const app = require('../shared/Express').app;
 const template = require('../shared/Express').template;
 const connection = require('../shared/Connection');
+const sha256 = require('js-sha256');
 
 app.put('/new_user', function (req, res) {
-    const net_id = req.body.net_id;
+    const net_id = req.body.netID;
     const password = req.body.password;
     const name = req.body.name;
     const classes = req.body.classes;
+    const hash = sha256(password + net_id);
 
     if (!net_id) {
         res.status(400).send("Please provide an net id parameter");
@@ -25,47 +27,44 @@ app.put('/new_user', function (req, res) {
         console.log('FAILED: GET ' + req.originalUrl);
         return;
     }
-
+    // TODO: Fix objects below
     var success = true;
     connection.query(
         `INSERT INTO User
-        VALUES('${net_id}', '${password}', '${name}');`, function (error, result) {
+        VALUES('${net_id}', '${name}', '${hash}');`, function (error, result) {
             if (error) {
-              var result = template;
-              result.status = 500;
-              result.success = false;
-              result.message = (error.sqlMessage ? error.sqlMessage : error);
-              res.status(500).send(result);
+              success = false;
+              template.status = 500;
+              template.success = success;
+              template.message = (error.sqlMessage ? error.sqlMessage : error);
+              console.log(template);
+              res.status(500).send(template);
               return;
             } else {
-                classes.forEach(cl => {
-                    console.log(cl);
-                    connection.query(
-                        `INSERT INTO UserClasses
-                        VALUES ('${net_id}', ${cl});`, function (error, result) {
-                          if (error) {
-                                success = false;
-                                console.log(result);
-                          }
+              classes.forEach(cl => {
+                  connection.query(
+                      `INSERT INTO UserClasses
+                      VALUES ('${net_id}', ${cl});`, function (error, result) {
+                        if (error) {
+                              success = false;
+                              template.status = 500;
+                              template.success = success;
+                              template.message = 'Error when inserting Classes. Check Classs array';
+                              console.log(template);
+                              res.status(500).send(template);
                         }
-                    );
-                });
+                      }
+                  );
+              });
+              template.status = 200;
+              template.success = true;
+              template.netID = net_id;
+              template.classes = classes;
+              template.name = name;
+              template.message = 'Account created';
+              res.status(200).send(template);
+              return;
             }
           }
     );
-
-    if (success) {
-        res.status(200).send({
-            message: 'Account created',
-            netID: net_id,
-            name: name,
-            classes: classes,
-            success: true,
-        });
-    } else {
-        res.status(500).send({
-            message: 'Something went wrong',
-            success: false,
-        });
-    }
 });
